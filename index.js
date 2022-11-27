865959f8fe63ab82c1c1a9b06f4f54de3f0db49f
Rodgers Ogada <ogadarodgers@gmail.com>
2022-11-27 14:46:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toLocaleUpperCase()
+}
+
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'betty';
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'hoo';
+}
